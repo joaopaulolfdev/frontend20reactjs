cf6ce0552fd8efe799c9f94d96542c73a3eec216
João Paulo Lima Ferreira <joaopaulolfdev@gmail.com>
2023-03-23 09:27:52 -0300

    new commit

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import './Header.css';
+
+
+function Header(props) {
+  return (
+    <div className="Header">
+      <div className="Header__logo">
+        <h2>Logo</h2> 
+      </div>
+    <div className="Header__menu">
+        <a href=" ">Home</a>
+        <a href=" ">Sobre</a>
+    </div>
+  
+    </div>
+  );
+}
+
+export default Header;
